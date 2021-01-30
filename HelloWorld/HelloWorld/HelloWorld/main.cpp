5069e8f4462b0e0021a2518122db624c64ea2ecc
Cesar Montero <cesar.montero@bytedance.com>
2021-01-29 21:39:04 -0800

    first commit
    first commit

---
+++ HelloWorld/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Cesar Montero on 1/29/21.
+//  Copyright © 2021 Cesar Montero. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
