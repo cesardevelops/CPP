70567fcc508a71b9c3e66878e9dcd61ad730c197
Cesar Montero <cesar.montero@bytedance.com>
2021-01-29 20:15:00 -0800

    first commit
    first commit

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,9 @@
+//
+//  HelloWorld.cpp
+//  
+//
+//  Created by Cesar Montero on 1/29/21.
+//
+
+#include "HelloWorld.hpp"
+
---
+++ HelloWorld/HelloWorld.hpp
@@ -0,0 +1,13 @@
+//
+//  HelloWorld.hpp
+//  
+//
+//  Created by Cesar Montero on 1/29/21.
+//
+
+#ifndef HelloWorld_hpp
+#define HelloWorld_hpp
+
+#include <stdio.h>
+
+#endif /* HelloWorld_hpp */
