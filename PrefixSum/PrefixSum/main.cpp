b240d025b798128924d7c0e9be151c64952d5451
Cesar Montero <cesar.montero@bytedance.com>
2021-03-21 13:56:39 -0700

    Cleaner solution
    I cleaned the solution a little bit

--- PrefixSum/PrefixSum/main.cpp
+++ PrefixSum/PrefixSum/main.cpp
@@ -4,41 +4,26 @@
 //
 //  Created by Cesar Montero on 3/20/21.
 //
+// element 1, prefixsum + inputArray[0]:
+// element 2, prefixsum + inputArray[1]
 
-
-#include <iostream>
 #include <array>
-using namespace std;
+#include <iostream>
 
-/*
- 
-{10, 30, 40, 45, 60}
 
- prefix sum = 0
- prefi sum = 10 = inputArray[0]
- prefix sum = 30 = inputArray[0] + inputArray[1] .... prefixsum += inputArray[i]
-
- */
-
-int main(int argc, const char * argv[])
+int main()
     {
-        
-        int inputArray[] = {10, 20, 10, 5, 15};
-        int inputSize = sizeof(inputArray)/sizeof(inputArray[0]);
-        int outputArray[inputSize];
+        int inputArray[] = {1, 2, 3, 4, 5};
+
+        int arraySize = sizeof(inputArray)/sizeof(inputArray[0]);
+        int outputArray[arraySize];
         int prefixsum = 0;
-        for(int i = 0; i < inputSize; i++)
+        
+        for(int i = 0; i < arraySize; i++)
             {
                 prefixsum += inputArray[i];
                 outputArray[i] = prefixsum;
+                std::cout << "outputArray[" << outputArray[i] << "]: " << outputArray[i] << std::endl;
             }
         
-        cout << "Prefix Sum: ";
-        for(int i = 0; i < inputSize; i++)
-            {
-                cout << outputArray[i] << ", ";
-            }
-        cout << endl;
-        
-        return 0;
     }
