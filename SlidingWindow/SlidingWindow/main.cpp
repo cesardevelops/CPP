46fb6b4a28632723290cbf7fd5ade771b263c1be
Cesar Montero <cesardevelops@gmail.com>
2021-03-28 15:34:48 -0700

    Sliding Window is complete

--- SlidingWindow/SlidingWindow/main.cpp
+++ SlidingWindow/SlidingWindow/main.cpp
@@ -6,9 +6,46 @@
 //
 
 #include <iostream>
+#include <unordered_map>
+#include <string>
 
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    string stringInput = "aaabcc";
+    unordered_map<char, int> um_letters;
+    
+    int k = 3;
+    int left = 0;
+    int right = left + k - 1;
+    bool move = true;
+
+    
+    while(move == true){
+        
+        for(int i = 0; i < stringInput.length(); i++){
+
+            //Print a line
+            if(i == left){cout << '[';}
+            cout << stringInput[i];
+            if(i == right){cout << ']';}
+
+            //Create unordered map set
+            if(i >= left && i <= right ){
+                um_letters[stringInput[i]] += 1;
+            }
+        }
+        cout << "-> "<< um_letters.size() << endl;
+        //clear the unordered map
+        um_letters.clear();
+        
+        //increase counters and evaluate if we should continue the loop
+        left++;
+        right = right = left + k - 1;
+        if(right == stringInput.length()){move = false;}//we reach the end of the array
+    }
+
     return 0;
 }
+
+// nlogn
